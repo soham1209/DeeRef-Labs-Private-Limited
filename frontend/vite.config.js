7658a6d36e4e5f192f4a0a302a41a081a19ec49c
soham1209 <sohamns1234@gmail.com>
2025-12-05 10:47:22 +0530

    feat: Add server configuration with API proxy settings in Vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api' :{
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    port: 3000,
+  },
   plugins: [react(), tailwindcss()],
 })
